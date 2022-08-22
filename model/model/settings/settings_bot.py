8b00e5cbf9bc9f2067f00bbfaaa4daa663b6f62b
yanapyata <105721676+yanapyata@users.noreply.github.com>
2022-08-22 19:29:57 +0300

    Create settings_bot.py

---
+++ model/model/settings/settings_bot.py
@@ -0,0 +1,7 @@
+import vk_api
+from vk_api.longpoll import VkLongPoll
+
+"""настроки для бота, создание таблиц, экземпляров"""
+
+from model.database.create_table import CreateTable
+from model.database.database import DataBase
