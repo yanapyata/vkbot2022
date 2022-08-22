4ca505990316534eb38f4b32a7d815954c31f4e9
yanapyata <105721676+yanapyata@users.noreply.github.com>
2022-08-22 19:25:44 +0300

    Create create_table.py

---
+++ model/database/create_table.py
@@ -0,0 +1,8 @@
+@@ -8,6 +8,7 @@ def __init__(self, db):
+        self.connections = self.engine.connect()
+
+    def create_table(self):
+        """создание таблиц в БД"""
+        self.connections.execute(
+            "create table if not exists Users (id serial primary key, users_id integer unique not null);")
+        self.connections.execute("create table if not exists UsersLikeList (id serial primary key, "
