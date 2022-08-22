fc8a44eea42fcace463d5f87bc01c71e6cd81d5e
yanapyata <105721676+yanapyata@users.noreply.github.com>
2022-08-22 19:29:12 +0300

    Create config.py

---
+++ model/database/model/settings/config.py
@@ -0,0 +1,7 @@
+@@ -1,6 +1,6 @@
+login = '<логин вк>'
+password = '<пароль вк>'
+token = '<токен сообщества>'
+db_bot = 'postgresql://postgres:<Пароль от пользователя postgres>@localhost:5432/vkbot'
+db_bot = 'postgresql://postgres:<Пароль от пользователя postgres>@localhost:5432/vkbot'  # база должна называться vkbot
+group_id = 0  # id сообщества поствавить свой
