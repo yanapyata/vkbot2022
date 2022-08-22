cdb766f6675ea2687a086db115cf8ed5479753d0
yanapyata <105721676+yanapyata@users.noreply.github.com>
2022-08-22 19:32:08 +0300

    Create regular_expression.py

---
+++ model/model/vk_user/regular_expression.py
@@ -0,0 +1,11 @@
+@@ -1,9 +1,10 @@
+import re
+PATTERNS_CITY = r'^\*[A-za-zА-я-а-я]+'
+
+
+def regular_search(patterns, text):
+    """Вернет правду если выражение верно, используется в поиске по параметрам"""
+    if re.search(patterns, text):
+        return True
+    else:
+        return False
