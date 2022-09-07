5232c3dffd89cdf811a9a7761d84d2fadb46ea7b
yanapyata <105721676+yanapyata@users.noreply.github.com>
2022-09-07 21:19:14 +0300

    Update create_table.py

--- model/database/create_table.py
+++ model/database/create_table.py
@@ -1,4 +1,10 @@
+import sqlalchemy
+
+
+class CreateTable:
     def __init__(self, db):
+        self.db = db
+        self.engine = sqlalchemy.create_engine(self.db)
         self.connections = self.engine.connect()
 
     def create_table(self):
@@ -6,3 +12,16 @@
         self.connections.execute(
             "create table if not exists Users (id serial primary key, users_id integer unique not null);")
         self.connections.execute("create table if not exists UsersLikeList (id serial primary key, "
+                                 "users_like integer unique not null, id_users integer not null references Users(id));")
+        self.connections.execute("create table if not exists UsersBlackList (id serial primary key, "
+                                 "users_black integer unique not null, "
+                                 "id_users integer not null references Users(id));")
+        self.connections.execute("create table if not exists AdvancedSearch ("
+                                 "id serial primary key, "
+                                 "city varchar(40) not null, "
+                                 "age_from integer not null,"
+                                 "age_to integer not null,"
+                                 "user_sex integer not null,"
+                                 "user_status integer not null,"
+                                 "id_users integer not null references Users(id));")
+        self.connections.close()
