5bb2a3c9c480c18b0981c9fe46bb680193a88a41
yanapyata <105721676+yanapyata@users.noreply.github.com>
2022-09-07 21:08:01 +0300

    Delete model/database/model directory

--- model/database/model/settings/config.py
+++
@@ -1,6 +0,0 @@
-login = '<логин вк>'
-password = '<пароль вк>'
-token = '<токен сообщества>'
-db_bot = 'postgresql://postgres:<Пароль от пользователя postgres>@localhost:5432/vkbot'
-db_bot = 'postgresql://postgres:<Пароль от пользователя postgres>@localhost:5432/vkbot'  # база должна называться vkbot
-group_id = 0  # id сообщества поствавить свой
